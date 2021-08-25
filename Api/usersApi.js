1f6d1547300382b1a606d7a3ea4e9ddd2c5f8c4d
liinh97 <nguyenvanlinh506@gmail.com>
2021-08-25 16:03:39 +0700

    m

--- Api/usersApi.js
+++ Api/usersApi.js
@@ -7,32 +7,34 @@ const validate = require("../Services/validator");
 
 router.post("/login", validate.validateUser(), async (req, res) => {
 
-    try {
+    res.json('nice');
 
-        const errors = validationResult(req).formatWith(validate.formatErrors);
-        if (!errors.isEmpty()) {
-            res.json(response(false, errors.array()[0]));
-        }else{
-            const data = new usersModels(
-                req.body.username,
-                req.body.password,
-            );
+    // try {
+
+    //     const errors = validationResult(req).formatWith(validate.formatErrors);
+    //     if (!errors.isEmpty()) {
+    //         res.json(response(false, errors.array()[0]));
+    //     }else{
+    //         const data = new usersModels(
+    //             req.body.username,
+    //             req.body.password,
+    //         );
     
-            const getData = await data.login();
-            if (getData.status) {
-                const checkData = data.verifyPassword(req.body.password, getData.data);
-                if (checkData) {
-                    const token = data.createToken();
-                    res.json(response(true, { "username": data.username, "token": token }));
-                } else {
-                    res.json(response(false, "Password not found, check again ^_^"));
-                }
-            } else {
-                res.json(response(false, "Username not found, check again >.<"));
-            }
-        }
+    //         const getData = await data.login();
+    //         if (getData.status) {
+    //             const checkData = data.verifyPassword(req.body.password, getData.data);
+    //             if (checkData) {
+    //                 const token = data.createToken();
+    //                 res.json(response(true, { "username": data.username, "token": token }));
+    //             } else {
+    //                 res.json(response(false, "Password not found, check again ^_^"));
+    //             }
+    //         } else {
+    //             res.json(response(false, "Username not found, check again >.<"));
+    //         }
+    //     }
 
-    } catch (err) { }
+    // } catch (err) { }
 
 });
 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require("http");
 const express = require("express");
-const socket = require("socket.io");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
@@ -8,25 +7,10 @@ const router = require("./Api/api");
 const { connectionMongo } = require("./Repositories/database");
 const server = http.createServer(app);
 
-connectionMongo();
 app.use(cors());
+connectionMongo();
 app.use(bodyParser());
 app.use(router);
-
-// Config socket.io
-const io = socket(server, {
-    cors: {
-        origin: "*",
-    },
-});
-
-// Connection socket.io
-io.on("connection", (socket) => {
-    console.log("Have a user join server");
-    socket.on("disconect", () => {
-        console.log("A user disconected!");
-    });
-});
 
 // server.listen("8080", err => {
 //     err ?? console.log("Server is running at http://localhost:8080");
