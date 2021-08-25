365208038bc1904d319397566fc2a99078a4d8ca
liinh97 <nguyenvanlinh506@gmail.com>
2021-08-25 15:18:36 +0700

    m

--- Api/api.js
+++ Api/api.js
@@ -4,10 +4,10 @@ const usersApi = require("./usersApi");
 const vocabularyApi = require("./vocabularyApi");
 const { authentication } = require("../Middleware/authentication");
 
-// router.use("/", usersApi);
+router.use("/", usersApi);
 
-// // Middleware User With Login Token
-// router.use(authentication);
-// router.use("/vocabulary", vocabularyApi);
+// Middleware User With Login Token
+router.use(authentication);
+router.use("/vocabulary", vocabularyApi);
 
 module.exports = router;
